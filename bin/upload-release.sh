3bfbb1d115624855bad10c5a7e9150272eb32ec0
geeksville <kevinh@geeksville.com>
2023-03-04 13:55:35 -0800

    I don't think setuptools needs flat text README anymore

--- bin/upload-release.sh
+++ bin/upload-release.sh
@@ -2,7 +2,7 @@ rm dist/*
 set -e
 
 bin/regen-docs.sh
-pandoc --from=markdown --to=rst --output=README README.md
+# pandoc --from=markdown --to=rst --output=README README.md
 python3 -m build
 python3 -m twine check dist/*
 python3 -m twine upload dist/*
